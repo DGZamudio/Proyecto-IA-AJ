e97ab1415e744e8a2cf225d984984539996c5b21
Giovanny <giovannyavj888@hotmail.com>
2025-01-17 16:02:51 -0500

    Mas

--- Propuesta 2 +/compilacion/js/mas-noticias.js
+++ Propuesta 2 +/compilacion/js/mas-noticias.js
@@ -1,14 +1,17 @@
 function desplegarMasNoticias() {
     var x = document.getElementById("contenedor-mas-noticias");
     var y = document.getElementById("apartado-mas-noticias");
+    var z = document.getElementById("ver-mas-noticias")
 
     if (x.style.display === "block") {
         x.style.display = "none";
         y.classList.remove("apartado-mas-noticias2");
         y.classList.add("apartado-mas-noticias");
+        z.style.display = "none";
     } else {
         x.style.display = "block";
         y.classList.remove("apartado-mas-noticias");
         y.classList.add("apartado-mas-noticias2");
+        z.style.display = "flex";
     }
 }
