8347489c8da0e1fe85fb46ea23a3d0a7f52506c9
Giovanny <giovannyavj888@hotmail.com>
2025-01-23 14:58:38 -0500

    Propuesta actual

--- Novedades/js/novedades_desplegar-mas-noticias.js
+++
@@ -1,9 +0,0 @@
-function desplegarMasNoticias() {
-    var x = document.getElementById("contenedor-noticias-ocultas");
-
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
--- Propuesta 2 +++/compilacion/js/popup_noticia.js
+++
@@ -1,8 +0,0 @@
-function desplegarNoticia() {
-    var x = document.getElementById("pop-up");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
